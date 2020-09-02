3519033b0aaa3230a68b8e3e09309f23f5ac03a0
unknown <kevinburon97@gmail.com>
2020-09-02 18:49:21 -0400

    Finished 2

--- Missions_to_Mars/mission_to_mars.py
+++ Missions_to_Mars/mission_to_mars.py
@@ -8,18 +8,21 @@ app=Flask(__name__)
 #Establish Mongo connection
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars")
-
 @app.route("/")
 def home():
+    return render_template("index.html")
+    
+@app.route("/data")
+def marsdata():
     data = mongo.db.nasa.find_one()
-    return  render_template("index.html", data=data)
+    return  render_template("index2.html", data=data)
 
 @app.route("/scrape")
 def scraper():
     nasa = mongo.db.nasa
     nasa_data = scrape()
     nasa.update({}, nasa_data, upsert=True)
-    return redirect("/")
+    return redirect("/data")
 
 
 
